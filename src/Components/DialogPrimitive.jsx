c54797436a425ae483934fcaad0fcb67b471bd40
AhmedTarek22 <ahmedtarekabdo22@gmail.com>
2025-02-26 14:22:50 +0200

    Completed board logic: add, edit, and delete functionalities

--- src/Components/DialogPrimitive.jsx
+++ src/Components/DialogPrimitive.jsx
@@ -1,6 +1,4 @@
 import * as Dialog from "@radix-ui/react-dialog";
-// import TextField from "./TextField";
-// import Button from "./Button";
 
 const DialogPrimitive = ({
   title,
@@ -12,34 +10,10 @@ const DialogPrimitive = ({
   <Dialog.Root open={isOpen} onOpenChange={setOpen}>
     <Dialog.Trigger asChild>{triggerComponent}</Dialog.Trigger>
     <Dialog.Portal>
-      <Dialog.Overlay className="fixed inset-0 bg-blackA6 data-[state=open]:animate-overlayShow" />
-      <Dialog.Content className="fixed left-1/2 top-1/2 max-h-[85vh] w-[90vw] max-w-[450px] -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none data-[state=open]:animate-contentShow">
-        <Dialog.Title className="mb-5 text-heading-xl font-jakarta">
-          {title}
-        </Dialog.Title>
+      <Dialog.Overlay className="data-[state=open]:animate-overlayShow fixed inset-0 bg-black/50" />
+      <Dialog.Content className="data-[state=open]:animate-contentShow fixed left-[50%] top-[50%] max-h-[85vh] w-[480px] translate-x-[-50%] translate-y-[-50%] overflow-auto rounded-[6px] bg-white p-8 focus:outline-none">
+        <Dialog.Title className="text-heading-l">{title}</Dialog.Title>
         {children}
-        {/* <fieldset className="mb-[15px] flex flex-col gap-2">
-          <label className="w-full text-[15px] text-violet11" htmlFor="name">
-            Name
-          </label>
-          <TextField placeholder="e.g. Web Design" />
-        </fieldset>
-        <fieldset className="mb-[15px] flex flex-col gap-2">
-          <label className="w-full text-[15px] text-violet11" htmlFor="name">
-            Columns
-          </label>
-          <div>
-            <TextField placeholder="e.g. Web Design" />
-          </div>
-        </fieldset>
-        <div className="mt-[25px] flex flex-col gap-3">
-          <Button isFullWidth={true} variant={"secondary"} size={"sm"}>
-            + Add New Column
-          </Button>
-          <Button isFullWidth={true} size={"sm"}>
-            Create New Board
-          </Button>
-        </div> */}
       </Dialog.Content>
     </Dialog.Portal>
   </Dialog.Root>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,24 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import DialogPrimitive from "./DialogPrimitive";
 import DropdownPrimitive from "./DropdownPrimitive";
 import iconVerticalEllipsis from "../assets/icon-vertical-ellipsis.svg";
+import { DataContext } from "../DataContext";
+import AddNewBoardForm from "./AddNewBoardForm";
+
 const Header = () => {
+  const { data, setData, selectedBoardIndex } = useContext(DataContext);
   const [open, setOpen] = useState(false);
 
-  const onEditBoard = () => setOpen(true);
+  const onEditBoard = () => {
+    setOpen(true);
+  };
+
+  const onDeleteBoard = () => {
+    if (window.confirm("Are you sure you want to delete this board?")) {
+      setData((prev) => prev.toSpliced(selectedBoardIndex, 1));
+    }
+  };
+  
   return (
     <header className="flex h-[97px] shrink-0 items-center">
       <div className="flex w-[300px] items-center gap-4 self-stretch border-b border-r border-lines-light pl-8 text-[32px] font-bold">
@@ -18,7 +31,7 @@ const Header = () => {
             edit: { label: "Edit Board", onClick: onEditBoard },
             delete: {
               label: "Delete Board",
-              onClick: () => console.log("Delete"),
+              onClick: () => onDeleteBoard(),
             },
           }}
           triggerComponent={() => (
@@ -27,11 +40,14 @@ const Header = () => {
             </button>
           )}
         />
-        <DialogPrimitive
-          isOpen={open}
-          setOpen={setOpen}
-          title="Edit Board"
-        ></DialogPrimitive>
+        <DialogPrimitive isOpen={open} setOpen={setOpen} title="Edit Board">
+          <AddNewBoardForm
+            boardId={data[selectedBoardIndex]?.id}
+            columns={data[selectedBoardIndex]?.columns}
+            title={data[selectedBoardIndex]?.title}
+            toggleDialog={setOpen}
+          />
+        </DialogPrimitive>
       </div>
     </header>
   );
