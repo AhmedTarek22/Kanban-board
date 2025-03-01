ec9710b958e27e53a9a52e6696d814c2b71be70f
AhmedTarek22 <ahmedtarekabdo22@gmail.com>
2025-03-01 14:06:52 +0200

    Completed task and column logic: add, edit, and delete tafunctionalities

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -5,11 +5,108 @@
  * @returns {JSX.Element}
  */
 
-const Card = ({ item }) => {
+import { useContext, useState } from "react";
+import { DataContext } from "../DataContext";
+
+const Card = ({ item, columnId }) => {
+  const { setData, selectedBoardIndex } = useContext(DataContext);
+  const [isEditMode, setIsEditMode] = useState(false);
+
+  const onDeleteHandler = () => {
+    if (window.confirm("Are you sure you want to delete this card?")) {
+      setData((prev) => {
+        const newData = [...prev];
+        const newColumns = newData[selectedBoardIndex].columns.map((column) => {
+          if (column.id === columnId) {
+            return {
+              ...column,
+              tasks: column.tasks.filter((task) => task.id !== item.id),
+            };
+          }
+          return column;
+        });
+        newData[selectedBoardIndex] = {
+          ...newData[selectedBoardIndex],
+          columns: newColumns,
+        };
+        return newData;
+      });
+    }
+  };
+
+  const toggleEditMode = () => {
+    setIsEditMode(true);
+  };
+
+  const onFocusHandler = (e) => {
+    e.target.select();
+  };
+
+  const onBlurHandler = (e) => {    
+    setIsEditMode(false);
+    console.log("test");
+    if (e.target.value.trim() === item.title) return;
+    setData((prev) => {
+      const newData = [...prev];
+      const newColumns = newData[selectedBoardIndex].columns.map((column) => {
+        if (column.id === columnId) {
+          console.log("true");
+          return {
+            ...column,
+            tasks: column.tasks.map((task) => {
+              if (task.id === item.id) {
+                console.log("true");
+                return {
+                  ...task,
+                  title: e.target.value.trim(),
+                };
+              }
+              return task;
+            }),
+          };
+        }
+        return column;
+      });
+
+      newData[selectedBoardIndex] = {
+        ...newData[selectedBoardIndex],
+        columns: newColumns,
+      };
+
+      return newData;
+    });
+  };
+
+  const onKeyDownHandler = (e) => {
+    (e.key === "Enter") && e.target.blur();
+  };
+
   return (
     <div className="group/card relative min-h-16 overflow-y-hidden rounded-lg bg-white px-4 py-3 shadow-sm">
-      <h2 className="text-heading-m">{item.title}</h2>
-      <p>{item.description}</p>
+      {isEditMode ? (
+        <textarea
+          className="h-full resize-none text-heading-m outline-light-grey"
+          defaultValue={item.title}
+          onFocus={onFocusHandler}
+          onBlur={onBlurHandler}
+          onKeyDown={onKeyDownHandler}
+          autoFocus
+        ></textarea>
+      ) : (
+        <button
+          onClick={toggleEditMode}
+          className="peer h-full text-start text-heading-m"
+        >
+          {item.title}
+        </button>
+      )}
+      {/* <p>{item.description}</p> */}
+      <button
+        className="absolute bottom-0 right-0 top-0 bg-white p-2 text-body-m text-red opacity-0 shadow duration-300 focus:opacity-100 group-hover/card:opacity-100 peer-focus:opacity-100"
+        onClick={onDeleteHandler}
+      >
+        Delete
+      </button>
     </div>
   );
 };
